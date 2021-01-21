268e4de7d2e1f811d2cf1da20234d9def429df3d
Alifahes <31044906+Alifahes@users.noreply.github.com>
2021-01-22 00:32:01 +0200

    Add files via upload

---
+++ p.js
@@ -0,0 +1,14 @@
+var a=document.getElementById("lp")
+    b=document.getElementById("s");
+
+a.onclick=()=>{
+    'use strict';
+    var b=document.getElementById("s");
+    if (b.style.display==="block"){
+              b.style.display="none";
+
+    }else{
+        b.style.display="block";
+    }
+
+}
